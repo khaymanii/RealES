f4167f841ade0d708d9834c56d8c885d50792447
khaymanii <kennethakpo61@gmail.com>
2025-01-01 23:27:09 +0100

    Add Roboto font and restructure layout components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,16 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./component/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        roboto: ["Roboto", ...defaultTheme.fontFamily.sans],
       },
     },
   },

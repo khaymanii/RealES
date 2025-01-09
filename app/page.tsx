c1834b74e953c7ce3723721f5ddea7d2659d0180
olanrewaju-07 <issabasheet@gmail.com>
2025-01-09 16:42:58 +0100

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import HeroSection from "@/component/Hero";
+import HomeCard from "@/component/HomeCard";
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return <div>
+    <HeroSection />
+    <HomeCard />
+  </div>;
 }
 
 export default page;
